52242d9c2e64ae01e8b5995f9858f6b49db146fc
Rapipat <rapipatu@cariva.co.th>
2023-06-18 12:06:52 +0700

    Update utility

--- pkg/v5/utility/utility.go
+++ pkg/v5/utility/utility.go
@@ -1,9 +1,31 @@
-package v5
+package utility
 
 import (
+	"net/url"
+
 	"github.com/yaihub/ndid-go-sdk/pkg/v5/utility/swagger"
 )
 
 type Utility struct {
-	APIClient *swagger.APIClient
+	APIClient   *swagger.APIClient
+	endpointUrl string
+}
+
+func NewUtility(endpointUrl string) (*Utility, error) {
+
+	u, err := url.Parse(endpointUrl)
+	if err != nil {
+		return nil, err
+	}
+
+	u.JoinPath("/utility")
+
+	swaggerCfg := swagger.NewConfiguration()
+	swaggerCfg.BasePath = u.Path
+	swaggerCfg.UserAgent = "ndid-go-sdk/0.1"
+
+	return &Utility{
+		APIClient:   swagger.NewAPIClient(swaggerCfg),
+		endpointUrl: endpointUrl,
+	}, nil
 }
